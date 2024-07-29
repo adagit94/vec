8947e759c48644ca7540b8054518f8151b7bd75c
Adam Pelc <sikulin94@gmail.com>
2024-07-29 10:34:02 +0200

    .

--- src/area.test.ts
+++ src/area.test.ts
@@ -1,8 +1,78 @@
 import { expect, test } from "@jest/globals"
-import { curvedArea } from "./area"
+import { curvedArea, density, densityPopulated } from "./area"
 
 test("area", () => {
-    expect(curvedArea([[0, 0], [1, 1]])).toBe(0.5)
-    expect(curvedArea([[0, 0], [1, 1], [2, 2]])).toBe(2)
-    expect(curvedArea([[0, 0], [1, 2], [2, 4], [3, 8]])).toBe(10)
+    expect(
+        curvedArea([
+            [0, 0],
+            [1, 1],
+        ])
+    ).toBe(0.5)
+
+    expect(
+        curvedArea([
+            [0, 0],
+            [1, 1],
+            [2, 2],
+        ])
+    ).toBe(2)
+
+    expect(
+        curvedArea([
+            [0, 0],
+            [1, 2],
+            [2, 4],
+            [3, 8],
+        ])
+    ).toBe(10)
+
+    expect(
+        density({
+            values: [
+                [1, 1],
+                [2, 1],
+            ],
+            width: 100,
+            height: 100,
+        })
+    ).toBe(0.0002)
+
+    expect(
+        density({
+            values: [
+                [1, 1],
+                [2, 1],
+                [4, 1],
+            ],
+            width: 100,
+            height: 100,
+            xInterval: [0, 2]
+        })
+    ).toBe(0.0004)
+
+    expect(
+        density({
+            values: [
+                [1, 1],
+                [2, 1],
+                [2, 2],
+            ],
+            width: 100,
+            height: 100,
+            yInterval: [0, 1]
+        })
+    ).toBe(0.0004)
+
+    expect(
+        densityPopulated({
+            values: [
+                [2, 2],
+                [2, 1],
+                [1, 1],
+                [1, 2],
+            ],
+            width: 100,
+            height: 100,
+        })
+    ).toBe(0.0016)
 })
--- src/area.ts
+++ src/area.ts
@@ -1,6 +1,6 @@
 export const curvedArea = (points: [number, number][]) => {
     let area = 0
-    
+
     for (let i = 1; i < points.length; i++) {
         const p1 = points[i - 1]
         const p2 = points[i]
@@ -25,66 +25,71 @@ export const curvedArea = (points: [number, number][]) => {
 }
 
 type Density = (params: {
-  values: [number, number][];
-  maxXVal: number;
-  maxYVal: number;
-  width: number;
-  height: number;
-  xInterval?: [number, number];
-  yInterval?: [number, number];
-}) => number;
+    values: [number, number][]
+    width: number
+    height: number
+    xInterval?: [number, number]
+    yInterval?: [number, number]
+}) => number
 
-export const density: Density = ({ values, xInterval, yInterval, maxXVal, maxYVal, width, height }) => {
-  const intervalValues =
-    xInterval === undefined && yInterval === undefined
-      ? values
-      : values.filter(
-          ([x, y]) =>
-            (xInterval === undefined || (x >= xInterval[0] && x <= xInterval[1])) &&
-            (yInterval === undefined || (y >= yInterval[0] && y <= yInterval[1])),
-        );
+export const density: Density = ({ values, xInterval, yInterval, width, height }) => {
+    const intervalValues =
+        xInterval === undefined && yInterval === undefined
+            ? values
+            : values.filter(
+                  ([x, y]) =>
+                      (xInterval === undefined || (x >= xInterval[0] && x <= xInterval[1])) &&
+                      (yInterval === undefined || (y >= yInterval[0] && y <= yInterval[1]))
+              )
 
-  const x1 = width * ((xInterval?.[0] ?? 0) / maxXVal);
-  const x2 = width * ((xInterval?.[1] ?? maxXVal) / maxXVal);
-  const y1 = height * ((yInterval?.[0] ?? 0) / maxYVal);
-  const y2 = height * ((yInterval?.[1] ?? maxYVal) / maxYVal);
+    let maxXVal = 0, maxYVal = 0
 
-  const intervalWidth = x2 - x1;
-  const intervalHeight = y2 - y1;
+    for (const [xVal, yVal] of values) {
+        maxXVal = Math.max(xVal, maxXVal)
+        maxYVal = Math.max(yVal, maxYVal)
+    }
 
-  const area = intervalWidth * intervalHeight;
-  const ratio = intervalValues.length / area;
+    const x1 = width * ((xInterval?.[0] ?? 0) / maxXVal)
+    const x2 = width * ((xInterval?.[1] ?? maxXVal) / maxXVal)
+    const y1 = height * ((yInterval?.[0] ?? 0) / maxYVal)
+    const y2 = height * ((yInterval?.[1] ?? maxYVal) / maxYVal)
 
-  return ratio;
-};
+    const intervalWidth = x2 - x1
+    const intervalHeight = y2 - y1
 
-type DensityPopulated = (params: { values: [number, number][]; width: number; height: number }) => number;
+    const area = intervalWidth * intervalHeight
+    const ratio = intervalValues.length / area
+
+    return ratio
+}
+
+type DensityPopulated = (params: { values: [number, number][]; width: number; height: number }) => number
 
 export const densityPopulated: DensityPopulated = ({ values, width, height }) => {
-  let minX = values[0][0];
-  let maxX = values[0][0];
-  let minY = values[0][1];
-  let maxY = values[0][1];
+    let minX = values[0][0]
+    let maxX = values[0][0]
+    let minY = values[0][1]
+    let maxY = values[0][1]
 
-  for (let i = 1; i < values.length; i++) {
-    const [xVal, yVal] = values[i];
+    for (let i = 1; i < values.length; i++) {
+        const [xVal, yVal] = values[i]
 
-    minX = Math.min(minX, xVal);
-    maxX = Math.max(maxX, xVal);
-    minY = Math.min(minY, yVal);
-    maxY = Math.max(maxY, yVal);
-  }
+        minX = Math.min(minX, xVal)
+        maxX = Math.max(maxX, xVal)
+        minY = Math.min(minY, yVal)
+        maxY = Math.max(maxY, yVal)
+    }
 
-  const minXCoord = width * (minX / maxX);
-  const maxXCoord = width;
-  const minYCoord = height * (minY / maxY);
-  const maxYCoord = height;
+    const minXCoord = width * (minX / maxX)
+    const maxXCoord = width
+    const minYCoord = height * (minY / maxY)
+    const maxYCoord = height
 
-  const intervalWidth = maxXCoord - minXCoord;
-  const intervalHeight = maxYCoord - minYCoord;
+    const intervalWidth = maxXCoord - minXCoord
+    const intervalHeight = maxYCoord - minYCoord
 
-  const area = intervalWidth * intervalHeight;
-  const ratio = values.length / area;
+    const area = intervalWidth * intervalHeight
+    const ratio = values.length / area
 
-  return ratio;
-};
+    return ratio
+}
